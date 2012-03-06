64bb1d1c0e394a895ae15ec7d5d0b9c5dad4f3b3
Kuzemko Alexandr <kuzemkoa@rambler.ru>
2012-03-06 22:12:38 +0200

    Add polytouch ft5x06 driver files.

---
+++ include/linux/input/edt-ft5x06.h
@@ -0,0 +1,17 @@
+#ifndef _EDT_FT5X06_H
+#define _EDT_FT5X06_H
+
+/*
+ * Copyright (c) 2011 Simon Budig, &lt;simon.budig@xxxxxxxxxxxxxxxxx&gt;
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ */
+
+struct edt_ft5x06_platform_data {
+	int irq_pin;
+	int reset_pin;
+};
+
+#endif /* _EDT_FT5X06_H */
