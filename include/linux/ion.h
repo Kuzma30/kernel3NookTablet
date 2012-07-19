b3478503066b6cc545c0256ec8e7a6aa0107ab94
Dominik Marszk <dmarszk@gmail.com>
2012-07-19 23:20:43 +0200

    Synced headers with 4AI1.6

--- include/linux/ion.h
+++ include/linux/ion.h
@@ -147,6 +147,7 @@ void ion_free(struct ion_client *client, struct ion_handle *handle);
 int ion_phys(struct ion_client *client, struct ion_handle *handle,
 	     ion_phys_addr_t *addr, size_t *len);
 
+
 /**
  * ion_phys_frm_dev - returns the physical address and len of a handle
  * @dev:	ion_dev
@@ -165,6 +166,7 @@ int ion_phys(struct ion_client *client, struct ion_handle *handle,
  */
 int ion_phys_frm_dev(struct ion_device *dev, struct ion_handle *handle,
 			ion_phys_addr_t *addr, size_t *len);
+
 
 /**
  * ion_map_kernel - create mapping for the given handle
