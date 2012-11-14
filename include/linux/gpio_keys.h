c7e7ac97b6cf2965a2e3d26437b0e9795608b633
Kuzemko Aleksandr <kuzemkoa@rambler.ru>
2012-11-14 07:33:24 +0200

    Workaround for BN home button resume (code from Nook HD). Update header

--- include/linux/gpio_keys.h
+++ include/linux/gpio_keys.h
@@ -12,6 +12,7 @@ struct gpio_keys_button {
 	int debounce_interval;	/* debounce ticks interval in msecs */
 	bool can_disable;
 	int value;		/* axis value for EV_ABS */
+	bool resume_event;	/* workaround for BN home button resume */
 };
 
 struct gpio_keys_platform_data {
