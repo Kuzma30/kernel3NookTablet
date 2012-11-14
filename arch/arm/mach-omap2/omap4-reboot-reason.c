a54689968b3fd5b2417c3375749a3ce67ce5d0c2
Kuzemko Aleksandr <kuzemkoa@rambler.ru>
2012-11-14 08:05:51 +0200

    Update reboot reason from Nook HD code.

--- arch/arm/mach-omap2/omap4-reboot-reason.c
+++ arch/arm/mach-omap2/omap4-reboot-reason.c
@@ -16,6 +16,7 @@
 #include <linux/platform_device.h>
 #include <linux/reboot.h>
 #include <linux/notifier.h>
+#include <linux/i2c/twl.h>
 
 #include <mach/hardware.h>
 #include <mach/omap4-common.h>
@@ -26,6 +27,7 @@ static int omap_reboot_notifier_call(struct notifier_block *this,
 {
 	void __iomem *sar_base;
 	char *reason = "normal";
+	unsigned char vmmc_val = 0;
 
 	sar_base = omap4_get_sar_ram_base();
 
@@ -33,17 +35,30 @@ static int omap_reboot_notifier_call(struct notifier_block *this,
 		return notifier_from_errno(-ENOMEM);
 
 	/* Save reboot mode in scratch memory */
-	if (code == SYS_RESTART){
-		if(cmd != NULL && strlen(cmd))
-			reason = cmd;
-		else
-			reason = "reboot";
-	}
+	if (code == SYS_RESTART && cmd != NULL && strlen(cmd))
+		reason = cmd;
 	else if (code == SYS_POWER_OFF)
 		reason = "off";
 
 	strncpy(sar_base + OMAP_REBOOT_REASON_OFFSET,
 			reason, OMAP_REBOOT_REASON_SIZE);
+
+	/*
+	  Set warm reset sensitivity bit
+	  and set VMMC voltage to 3.0V before
+	  rebooting
+	*/
+
+	twl_i2c_write_u8(TWL6030_MODULE_ID0, 0x95, 0x9B);
+	do {
+		/*
+		  Wait for the value to be written in the
+		  VMMC_CFG_VOLTAGE register
+		*/
+		twl_i2c_read_u8(TWL6030_MODULE_ID0, &vmmc_val, 0x9B);
+	} while (0x95 != vmmc_val);
+
+
 	return NOTIFY_DONE;
 }
 
